daf316e9bf8326ccabb7f02164b5f4df87bb18a3
Siobhán Schnittger <Siobhan.Schnittger@gmail.com>
2013-07-24 12:29:53 +0100

    Initial Commit

---
+++ get-tweets1.0.php
@@ -0,0 +1,28 @@
+<?php 
+//code from The Web Dev Door by Tom Elliott
+//Adapted by Siobhan Schnittger 
+//http://www.webdevdoor.com/php/authenticating-twitter-feed-timeline-oauth/
+
+session_start();
+require_once("twitteroauth/twitteroauth.php"); //Path to twitteroauth library
+ 
+$twitteruser = "thisdigitalinc";
+$notweets = 30;
+$consumerkey = "oXRHpijPXqkmpI01vB3XKQ";
+$consumersecret = "EBxsXvSZaDeiN08kHHtWaiiZyiGOdpsIP0UGBwy2g";
+$accesstoken = "1615587769-mhzmHR2bWQVlueppiW1mjwkrGMdGdw4qmoC6IMU";
+$accesstokensecret = "AcKfb0CjRi3mun0dpFQAhubh4Br8hLmlwac8G4IDJE";
+ 
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+ 
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+ 
+$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+ echo json_encode($tweets);
+?>
+
+
+
